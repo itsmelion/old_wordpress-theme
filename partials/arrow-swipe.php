22966bb7c4b13ebd58bf600c471a03b9dea5e855
christhopher.leao <christhopherleao@icloud.com>
2017-09-15 15:56:25 -0300

    clean cache

--- partials/arrow-swipe.php
+++
@@ -1,3 +0,0 @@
-<div class="text-center arrow light up">
-	<img src="<?php echo get_bloginfo('template_url') ?>/images/icons/arrow.svg" alt="Swipe up"/>
-</div>
--- single.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-
-<?php if ( have_posts() ) : the_post();
-	get_template_part( 'loop', 'overview' ); ?>
-	
-<?php else : get_template_part( 'loop', 'empty' ); endif;	?>
-
-<?php get_footer(); ?>
